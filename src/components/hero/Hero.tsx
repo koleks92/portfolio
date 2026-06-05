e5ecda34e1ec3e0699122d3503ed2df13942d3f2
Jan Konieczek <koleks92@gmail.com>
2026-06-05 20:16:24 +0200

    feat: Hero init

---
+++ src/components/hero/Hero.tsx
@@ -0,0 +1,18 @@
+import Page from "../UI/Page";
+
+
+export default function Hero() {
+    return (
+        <Page id="hero">
+            <div className="flex flex-1 flex-col justify-center items-center bg-dark-mid text-text-muted">
+                <div>Foto</div>
+                <span>Web & Mobile Developer</span>
+                <span>
+                    Web and Mobile Developer specializing in React and React
+                    Native, building fast, scalable, and user-focused
+                    applications with modern technologies.
+                </span>
+            </div>
+        </Page>
+    );
+}
