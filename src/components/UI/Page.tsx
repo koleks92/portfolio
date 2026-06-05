caa5ba45a6aa288af0f16d00cc891c1a9f9f2f48
Jan Konieczek <koleks92@gmail.com>
2026-06-05 20:18:00 +0200

    feat: Added id to props

--- src/components/UI/Page.tsx
+++ src/components/UI/Page.tsx
@@ -1,7 +1,8 @@
 type PageProps = {
+    id: string;
     children: React.ReactNode;
 };
 
-export default function Page({ children }: PageProps) {
-    return <div className="flex min-h-screen">{children}</div>;
+export default function Page({ id, children }: PageProps) {
+    return <div id={id} className="flex min-h-screen">{children}</div>;
 }
