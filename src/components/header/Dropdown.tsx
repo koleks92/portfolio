520112fc1905d2cc7d2c621360c055ad0f6fa047
Jan Konieczek <koleks92@gmail.com>
2026-06-05 20:17:41 +0200

    fix: Removed gallery

--- src/components/header/Dropdown.tsx
+++ src/components/header/Dropdown.tsx
@@ -5,7 +5,6 @@ type LinksType = {
 
 const links: LinksType[] = [
     { name: "Hero", href: "#hero" },
-    { name: "Apps", href: "#gallery" },
     { name: "App1", href: "#app1" },
     { name: "App2", href: "#app2" },
     { name: "App3", href: "#app3" },
