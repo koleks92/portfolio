0141fe8d8c3dcaaab38a4ff33cd5fb50f151149c
Jan Konieczek <koleks92@gmail.com>
2026-06-05 21:18:32 +0200

    fix: Removed id

--- src/components/hero/Hero.tsx
+++ src/components/hero/Hero.tsx
@@ -3,7 +3,7 @@ import Page from "../UI/Page";
 
 export default function Hero() {
     return (
-        <Page id="hero">
+        <Page>
             <div className="flex flex-1 flex-col justify-center items-center bg-dark-mid text-text-muted">
                 <div>Foto</div>
                 <span>Web & Mobile Developer</span>
