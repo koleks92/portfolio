41d964ce440ea6297085a2310f6ef88a1ac35265
Jan Konieczek <koleks92@gmail.com>
2026-06-15 13:17:12 +0200

    feat: Data template for apps

---
+++ src/data/apps.ts
@@ -0,0 +1,57 @@
+type AppData = {
+    id: string;
+    title: string;
+    description1: string;
+    description2: string;
+    image1: string;
+    image2: string;
+    color: "dark" | "light";
+};
+
+export const apps: AppData[] = [
+        {
+        id: "app1",
+        title: "App 1",
+        description1: "Some description about app 1",
+        description2: "Some description about app 2",
+        image1: "../assets/images/apps/temp.png",
+        image2: "../assets/images/apps/temp.png",
+        color: "light",
+    },
+    {
+        id: "app2",
+        title: "App 2",
+        description1: "Some description about app 1",
+        description2: "Some description about app 2",
+        image1: "../assets/images/apps/temp.png",
+        image2: "../assets/images/apps/temp.png",
+        color: "dark",
+    },
+    {
+        id: "app3",
+        title: "App 3",
+        description1: "Some description about app 1",
+        description2: "Some description about app 2",
+        image1: "../assets/images/apps/temp.png",
+        image2: "../assets/images/apps/temp.png",
+        color: "light",
+    },
+    {
+        id: "app4",
+        title: "App 4",
+        description1: "Some description about app 1",
+        description2: "Some description about app 2",
+        image1: "../assets/images/apps/temp.png",
+        image2: "../assets/images/apps/temp.png",
+        color: "dark",
+    },
+    {
+        id: "app5",
+        title: "App 5",
+        description1: "Some description about app 1",
+        description2: "Some description about app 2",
+        image1: "../assets/images/apps/temp.png",
+        image2: "../assets/images/apps/temp.png",
+        color: "light",
+    },
+]
