60f29abdb85e70d2db5cca7a625c40857600b721
Jan Konieczek <koleks92@gmail.com>
2026-07-13 21:47:09 +0200

    feat: Added pages and routing

--- src/App.tsx
+++ src/App.tsx
@@ -1,48 +1,14 @@
-import Header from "./components/header/Header";
-import Hero from "./components/hero/Hero";
-import AOS from "aos";
-import AppSection from "./components/appSection/AppSection";
-import { useEffect, useState } from "react";
-import IntroAnimation from "./components/UI/IntroAnimation";
-import "./styles/aos.css";
-import { apps } from "./data/apps";
+import { Routes, Route } from 'react-router-dom'
+import Home from './pages/Home'
+import Admin from './pages/Admin'
 
 function App() {
-    const [split, setSplit] = useState(false);
-
-    useEffect(() => {
-        AOS.init({
-            duration: 800, // animation duration in ms
-        });
-    }, [split]);
-
-    return (
-        <>
-            <IntroAnimation split={split} onComplete={() => setSplit(true)} />
-            <div
-                className={`min-h-screen flex flex-col transition-opacity duration-700 ${
-                    split ? "opacity-100" : "opacity-0"
-                }`}
-            >
-                <Header>Jan Sebastian Konieczek</Header>
-                <main className="flex-1">
-                    <div id="hero" className="scroll-mt-15">
-                        <Hero />
-                    </div>
-                    {apps.map((app) => (
-                        <div
-                            key={app.id}
-                            id={app.id}
-                            className={`scroll-mt-15 w-full ${app.color === "dark" ? "bg-dark-mid" : "bg-light"}`}
-                        >
-                            <AppSection data={app} />
-                        </div>
-                    ))}
-                    <section id="footer">Footer</section>
-                </main>
-            </div>
-        </>
-    );
+  return (
+    <Routes>
+      <Route path="/" element={<Home />} />
+      <Route path="/admin" element={<Admin />} />
+    </Routes>
+  )
 }
 
-export default App;
+export default App
