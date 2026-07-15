060863042efc794bd8e0f692f37845513eb7447e
Jan Konieczek <koleks92@gmail.com>
2026-07-15 15:28:59 +0200

    style: Styling

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,6 @@
-import { Routes, Route } from 'react-router-dom'
-import Home from './pages/Home'
-import Admin from './pages/Admin'
+import { Routes, Route } from "react-router-dom";
+import Home from "./pages/Home";
+import Admin from "./pages/Admin";
 
 function App() {
   return (
@@ -8,7 +8,7 @@ function App() {
       <Route path="/" element={<Home />} />
       <Route path="/admin" element={<Admin />} />
     </Routes>
-  )
+  );
 }
 
-export default App
+export default App;
