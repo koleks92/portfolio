e75cb6c748eb4ef2b1b07682449c3eee303077bb
Jan Konieczek <koleks92@gmail.com>
2026-07-20 21:43:55 +0200

    feat: Added route /add_app

--- src/App.tsx
+++ src/App.tsx
@@ -2,6 +2,7 @@ import { Routes, Route } from "react-router-dom";
 import Home from "./pages/Home";
 import Login from "./pages/Login";
 import Dashboard from "./pages/Dashboard";
+import AddApp from "./pages/AddApp";
 
 function App() {
   return (
@@ -9,6 +10,7 @@ function App() {
       <Route path="/" element={<Home />} />
       <Route path="/login" element={<Login />} />
       <Route path="/dashboard" element={<Dashboard />} />
+      <Route path="/add_app" element={<AddApp />} />
     </Routes>
   );
 }
