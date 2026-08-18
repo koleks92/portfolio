93f1dcba7675bc6fe20ae47dd1f730297e3b79f9
Jan Konieczek <koleks92@gmail.com>
2026-08-18 21:26:47 +0200

    fix: z-0 on Rays fix

--- src/components/hero/Hero.tsx
+++ src/components/hero/Hero.tsx
@@ -88,7 +88,7 @@ export default function Hero() {
 
   return (
     <>
-      <div className="fixed inset-0 z-10">
+      <div className="fixed inset-0 z-0">
         <LightRays
           raysOrigin="top-left"
           raysColor="#ffffff"
