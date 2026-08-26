899414cea1859a7f22fb9b0314991bc0b2e8d346
Jan Konieczek <koleks92@gmail.com>
2026-08-26 21:48:04 +0200

    feat: Added apps

--- src/components/gallery/GalleryHero.tsx
+++ src/components/gallery/GalleryHero.tsx
@@ -1,6 +1,14 @@
 import { useEffect, useState } from "react";
 import "../../styles/gallery.css";
-import temp from "../../assets/images/apps/temp.png";
+import avocadoro from "../../assets/images/apps/avocadoro/avocadoro1.png";
+import dishdate from "../../assets/images/apps/dishdate/dishdate_wide.png";
+import beatit from "../../assets/images/apps/beatit/beatit_wide.png";
+
+type App = {
+  name: string;
+  description: string;
+  src: string;
+};
 
 export default function GalleryHero() {
   const [visibleIndex, setVisibleIndex] = useState<number>(0);
@@ -15,29 +23,45 @@ export default function GalleryHero() {
     };
   }, [visibleIndex]);
 
-  const images: string[] = [temp, temp, temp];
+  const apps: App[] = [
+    {
+      name: "Avocadoro",
+      description: "Avocadoro App",
+      src: avocadoro,
+    },
+    {
+      name: "DishDate",
+      description: "DishDate App",
+      src: dishdate,
+    },
+    {
+      name: "BeatIt",
+      description: "BeatIt App",
+      src: beatit,
+    },
+  ];
 
   const nextIndex = () => {
-    setVisibleIndex((prev) => (prev === images.length - 1 ? 0 : prev + 1));
+    setVisibleIndex((prev) => (prev === apps.length - 1 ? 0 : prev + 1));
   };
 
   const previousIndex = () => {
-    setVisibleIndex((prev) => (prev === 0 ? images.length - 1 : prev - 1));
+    setVisibleIndex((prev) => (prev === 0 ? apps.length - 1 : prev - 1));
   };
 
   return (
     <div className="flex flex-col w-full h-full justify-center items-center z-2">
       <div className="flex justify-center items-center relative w-full h-full">
-        {images.map((item, index) => (
+        {apps.map((item, index) => (
           <img
-            src={item}
-            alt="Temp"
+            src={item.src}
+            alt={item.name}
             key={index}
             className={`imageCustom ${index === visibleIndex ? "visible" : "notVisible"}`}
           />
         ))}
       </div>
-      <div>
+      <div className="flex w-full flex-1 justify-around">
         <button onClick={() => previousIndex()} className="mx-4 text-3xl ">
           &lt;
         </button>
