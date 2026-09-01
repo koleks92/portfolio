fd1a2da75bfee4ddd8b3892c96a0c2a43add717c
koleks92_NEXT <koleks92@gmail.com>
2026-09-01 12:12:58 +0200

    fix: Moved to another folder

--- src/components/footer/Footer.tsx
+++ src/components/footer/Footer.tsx
@@ -1,15 +1,11 @@
 import Page from "../UI/Page";
 import CloudCombined from "./CloudCombined";
-import CloudWithRain from "./CloudWithRain";
-import CloudWithSun from "./CloudWithSun";
 
 export default function Footer() {
   return (
     <Page>
       <div className="flex h-screen flex-col overflow-hidden bg-dark-mid text-text-muted">
         <div className="flex flex-col">
-          {/* <CloudWithSun />
-          <CloudWithRain /> */}
           <CloudCombined />
         </div>
       </div>
